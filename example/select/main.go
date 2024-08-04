4929924e51fcbdbd46305829fb4610aaf61932b7
tomato3713 <tomato3713@outlook.com>
2024-08-04 15:17:31 +0900

    add comment

--- example/select/main.go
+++ example/select/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/tomato3713/linq"
 	"slices"
-	"fmt"
 )
 
 func main() {
@@ -12,13 +12,22 @@ func main() {
 	cond1 := func(v int) bool { return v > 5 }
 	cond2 := func(v int) bool { return v < 30 }
 
-	cmp := func(a, b int) int { return a-b }
+	cmp := func(a, b int) int { return a - b }
 
-	selector := func(item int) int { return item*2 }
+	selector := func(item int) int { return item * 2 }
 
 	q := linq.New(slices.Values(list))
 
-	for v := range q.Where(cond1).Where(cond2).OrderBy(cmp).Select(selector) {
+	for v := range q.
+		Where(cond1).      // -> []int{10, 33, 12, 11, 15}
+		Where(cond2).      // -> []int{10, 12, 11, 15}
+		OrderBy(cmp).      // -> []int{10, 11, 12, 15}
+		Select(selector) { // -> []int{20, 22, 24, 30}
+		// output
+		// 20
+		// 22
+		// 24
+		// 30
 		fmt.Println(v)
 	}
 }
